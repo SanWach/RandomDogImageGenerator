5ddb057a6e3a876459afe5ad1abc7602b770b505
SandroW <sandrowaechter@protonmail.com>
2024-02-25 14:05:58 +0100

    Javascript changes: Download Button works now

--- script.js
+++ script.js
@@ -13,31 +13,40 @@ document.getElementById('dogButton').addEventListener('click', function() {
         .catch(error => console.error('Error:', error));
 });
 
-document.getElementById('downloadButton').addEventListener('click', function() {
-    const dogImage = document.querySelector('#dogImageContainer img');
-    if (dogImage) {
-        
-        const imageSource = dogImage.src.replace(/^http:/, 'https:'); 
-        fetch(imageSource, {
-            method: 'GET',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'image/jpeg',
-            },
-        })
-        .then(response => response.blob())
-        .then(blob => {
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = 'dog-image.jpg'; 
-            a.click();
-            window.URL.revokeObjectURL(url); 
-        });
+document.getElementById('dogButton').addEventListener('click', function() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => {
+            const dogImageContainer = document.getElementById('dogImageContainer');
+            const imageElement = document.createElement('img');
+            imageElement.src = data.message;
+            imageElement.alt = "A random dog image";
+            
+            
+            dogImageContainer.innerHTML = '';
+            dogImageContainer.appendChild(imageElement);
 
-        
-        this.innerText = "Wooff!";
-    }
+            
+            const downloadButton = document.getElementById('downloadButton');
+            downloadButton.style.display = 'inline-block'; // 
+            downloadButton.innerText = "Download"; // 
+
+
+            downloadButton.onclick = function() {
+                const link = document.createElement('a');
+                link.href = imageElement.src;
+                link.download = 'dog-image.jpg'; // 
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+                downloadButton.innerText = "Wooff!";
+            };
+        })
+        .catch(error => {
+            console.error('Error fetching image:', error);
+            
+        });
 });
+
 
 //Sorry, i was too lazy for comments //
